0023a6afde3bc7fdfc50fcc20ec73f8a01544af4
VladimirAkopyan <Vladimir@akopyan.me>
2018-02-11 18:05:56 +0000

    Made the server send a heartbeat every second

--- MQTTserver/Program.cs
+++ MQTTserver/Program.cs
@@ -1,16 +1,14 @@
 ﻿using MQTTnet;
 using MQTTnet.Server;
 using System;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MQTTserver
 {
     public class Util
     {
-        /// <summary>
-        /// Adds two Numbers. Unit test this! 
-        /// </summary>
-        /// <returns>Summ of A and B</returns>
         public static int TestMeIAddNumbers(int A, int B)
         {
             return A + B;
@@ -34,12 +32,34 @@ namespace MQTTserver
             var mqttServer = new MqttFactory().CreateMqttServer();
             await mqttServer.StartAsync(optionsBuilder.Build());
             Console.WriteLine("Type 'quit' to exit");
+
+            var ct = new CancellationTokenSource(); ;
+            var heartbeatTask = Task.Run(async () => await ServerHeartbeat(ct.Token, mqttServer)); 
             while (true)
             {
                 if (Console.ReadLine().Contains("quit"))
                     break; 
             }
+            ct.Cancel();
+            await heartbeatTask; 
             await mqttServer.StopAsync();
+        }
+
+        private static async Task ServerHeartbeat(CancellationToken token, IMqttServer server)
+        {
+            long heartbeat =0;
+            while (token.IsCancellationRequested == false)
+            {
+                var message = new MqttApplicationMessageBuilder()
+                    .WithTopic("heartbeat")
+                    .WithPayload($"I am alive for {heartbeat} seconds")
+                    .WithAtMostOnceQoS()
+                    .WithRetainFlag(false)
+                    .Build();
+                await server.PublishAsync(message);
+                await Task.Delay(1000);
+                heartbeat++;
+            }
         }
     }
 }
